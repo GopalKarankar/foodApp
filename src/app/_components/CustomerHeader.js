91674a42bea428857eb243824314cd8c15293e1b
Gopal Karankar <karankargopal0gmail.com>
2026-01-31 13:48:21 +0530

    done

--- src/app/_components/CustomerHeader.js
+++ src/app/_components/CustomerHeader.js
@@ -45,7 +45,9 @@ const CustomerHeader = ({cartData2}) => {
 
                 // console.log(userStorage?._id);
 
-                const res = await fetch("https://food-app-lg35.vercel.app/api/cart/"+userStorage?._id);
+                const res = await fetch("https://food-app-lg35.vercel.app/api/cart/"+userStorage?._id,{
+                    cache:"no-store,"
+                });
 
                 const cartData = await res.json();
 
